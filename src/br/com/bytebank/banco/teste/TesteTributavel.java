c733e33e4a0dfa58e08b0d6220e13674379bda85
MateusBorges07 <mateusinfo36@gmail.com>
2023-02-08 11:24:04 -0300

    correção de erros

--- src/br/com/bytebank/banco/teste/TesteTributavel.java
+++ src/br/com/bytebank/banco/teste/TesteTributavel.java
@@ -1,14 +1,15 @@
 package br.com.bytebank.banco.teste;
 
 import br.com.bytebank.banco.modelo.CalculadorDeImposto;
+import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.SeguroDeVida;
 
 public class TesteTributavel {
     public static void main(String[] args) {
-         modelo.ContaCorrente cc = new modelo.ContaCorrente( 222, 333);
+         ContaCorrente cc = new ContaCorrente( 222, 333);
          cc.deposita(100.0);
 
-         modelo.SeguroDeVida seguro = new SeguroDeVida();
+         SeguroDeVida seguro = new SeguroDeVida();
 
         CalculadorDeImposto colc = new CalculadorDeImposto();
         colc.registra(cc);
